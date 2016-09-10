cc38ecbf588a36c460dcc930cbe6933575978cff
muuzakiiy <hamdiahmadi1504@gmail.com>
2016-09-10 20:56:38 +0700

    socket io

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+import socketio
+import eventlet
+import eventlet.wsgi
+from flask import Flask, render_template
 
 app = Flask(__name__)
+sio = socketio.Server()
 
 @app.route("/")
-def hello():
-		return "Hello !!"
+def  index():
+		return render_template('index.html')
+
+@sio.on("connect", namespace="/chat")
+def connect(sid, env):
+	print("connect ", sid)
+	sio.emit("connect", "you're connected", room = sid)
+
 
 if __name__ == '__main__':
-	app.run()
+
+	app = socketio.Middleware(sio, app)
+
+	eventlet.wsgi.server(eventlet.listen(("", 5000)), app)
